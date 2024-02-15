2c97b477abbe7e1b43885d6870a5ebbf6f4545d3
Ioanna-Maria Lygatsika <ioligatsika@outlook.com>
2024-02-15 18:17:44 +0100

    toy energy is weird

--- toy.py
+++ toy.py
@@ -6,14 +6,24 @@ from pyscf import gto
 
 coord = 'system/h2o.xyz'
 
+print('cc-pv5z')
+input_basis = {
+        'H': gto.basis.load('dat/cc-pv5z.nw', 'H'),
+        'O': gto.basis.load('dat/cc-pv5z.nw', 'O')
+        }
+mol = gto.M(atom=coord, basis=input_basis, unit='A')
+print(mol.nbas)
+mf = mol.HF()
+mf.kernel()
+
 print('6-31g')
 input_basis = {
         'H': gto.basis.load('dat/6-31g.nw', 'H'),
         'O': gto.basis.load('dat/6-31g.nw', 'O')
         }
-mol = gto.M(atom=coord, basis=input_basis)
+mol = gto.M(atom=coord, basis=input_basis, unit='A')
 print(mol.nbas)
-mf = mol.RHF()
+mf = mol.HF()
 mf.kernel()
 
 ao_nw = 'dat/test.nw'
@@ -22,9 +32,9 @@ input_basis = {
         'H': gto.basis.load(ao_nw, 'H'),
         'O': gto.basis.load(ao_nw, 'O')
         }
-mol = gto.M(atom=coord, basis=input_basis)
+mol = gto.M(atom=coord, basis=input_basis, unit='A')
 print(mol.nbas)
-mf = mol.RHF()
+mf = mol.HF()
 mf.kernel()
 
 
